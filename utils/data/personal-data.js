dce7e2887f38268f35d7aa13ce970ccc5f5999ed
Mangesh Tayade <mtayade91@gmail.com>
2024-03-18 12:43:06 -0700

    my changes

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,7 +1,7 @@
 export const personalData = {
   name: "MANGESH TAYADE",
   profile: '/profile.png',
-  designation: "Fornt End Developer",
+  designation: "Front End Developer",
   description: "My name is Mangesh Tayade. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'mtayade91@gmail.com',
   phone: '+917030707022',
